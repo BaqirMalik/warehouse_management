37a8970a574c4cbaa050e3343b51575ee6abc6ad
Muhammad Baqir <Muhammad Baqir@oddo.crecentech.co>
2024-07-02 06:33:51 -0500

    add groups

---
+++ ct_hr_integration/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "CT Inventory Consumption",
+    'summary': "Adjust the inventory in warehouse on consumption",
+    'description': """
+        This Application Is developed to Customize the HR Module in Odoo17.
+    """,
+    'author': "Crecentech",
+    'website': "https://crecentech.com/",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['website'],
+    'data': [
+        'views/menus.xml',
+    ],
+}
+
