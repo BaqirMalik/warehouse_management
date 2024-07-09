af1753e82061be2e67ac060cc64852a5d8a0e874
Muhammad Baqir <Muhammad Baqir@oddo.crecentech.co>
2024-07-09 11:36:58 -0500

    Fix Security Access rights on Employees Menu

--- ct_hr_integration/__manifest__.py
+++ ct_hr_integration/__manifest__.py
@@ -9,8 +9,10 @@
     'website': "https://crecentech.com/",
     'category': 'Uncategorized',
     'version': '0.1',
-    'depends': ['website','hr_recruitment'],
+    'depends': ['base','website','hr_recruitment'],
     'data': [
+        'security/user_groups.xml',
+        'security/ir.model.access.csv',
         'data/send_email_for_job_application.xml',
         'views/menus.xml',
         'views/inherit_hr_job_views.xml',
