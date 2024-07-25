31ace52fdf93f3b962d99549946153346e647fd9
Muhammad Baqir <Muhammad Baqir@oddo.crecentech.co>
2024-07-25 08:36:29 -0500

    Update Validation

--- ct_inventory_consumption/models/inherit_purchase_order.py
+++ ct_inventory_consumption/models/inherit_purchase_order.py
@@ -8,8 +8,9 @@ class InheritPurchaseOrder(models.Model):
 
     def button_confirm(self):
         for order in self:
+            if not order.order_line:
+                raise ValidationError(_("Please Select At Least One Product in the Order Lines."))
             for line in order.order_line:
                 if line.price_unit <= 0:
-                    raise ValidationError(
-                        _("Please Enter a Price Greater than Zero for All Order Lines Before Confirming the Order."))
+                    raise ValidationError(_("Please Enter a Price Greater than 0 for All Order Lines Before Confirming the Order."))
         return super(InheritPurchaseOrder, self).button_confirm()
